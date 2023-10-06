84927ea4917c1a893accb72828cd5252b47a88ee
Jace Babin <jbabin91@gmail.com>
2023-10-06 15:50:39 -0700

    init project
    Signed-off-by: Jace Babin <jbabin91@gmail.com>

--- e2e/src/example.spec.ts
+++ e2e/src/example.spec.ts
@@ -1,8 +1,16 @@
-import { test, expect } from '@playwright/test';
+import { expect, test } from '@playwright/test';
 
 test('has title', async ({ page }) => {
   await page.goto('/');
 
   // Expect h1 to contain a substring.
-  expect(await page.locator('h1').innerText()).toContain('Welcome');
+  expect(await page.locator('h1').textContent()).toContain('Welcome');
+});
+
+test('has link on page', async ({ page }) => {
+  await page.goto('/');
+
+  await expect(page.getByRole('link', { name: "What's next?" })).toContainText(
+    "What's next?"
+  );
 });
--- src/app/app.spec.tsx
+++ src/app/app.spec.tsx
@@ -10,6 +10,6 @@ describe('App', () => {
 
   it('should have a greeting as the title', () => {
     const { getByText } = render(<App />);
-    expect(getByText(/Welcome nx-react-vite-starter-standalone/gi)).toBeTruthy();
+    expect(getByText(/welcome nx-react-vite-starter-standalone/i)).toBeTruthy();
   });
 });
